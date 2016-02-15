5c0146ab9cadd1efd10011137dadcf3ddcb03a1e
Damir Arbula <damir.arbula@gmail.com>
2016-02-15 15:55:13 +0100

    Django 1.7 migrations

--- cmsplugin_markdown/widgets.py
+++ cmsplugin_markdown/widgets.py
@@ -1,6 +1,6 @@
 from django.forms.widgets import Textarea
 from django.utils.html import format_html
-from django.forms.util import flatatt
+from django.forms.utils import flatatt
 from django.utils.encoding import force_text
 
 
